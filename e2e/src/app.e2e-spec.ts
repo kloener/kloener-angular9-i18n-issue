7cf48fda747ea3f453653290990972789cd131ab
Christian Illies <c.illies@finatix.de>
2020-07-16 14:31:48 +0200

    rename project to kloener-angular9-i18n-issue

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular9-test app is running!');
+    expect(page.getTitleText()).toEqual('kloener-angular9-i18n-issue app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular9-test'),
+      dir: require('path').join(__dirname, './coverage/kloener-angular9-i18n-issue'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
